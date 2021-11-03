a9dc698b1bbf8f5e65aa9598ef1d98ec43afe296
1050575224@qq.com <1050575224@qq.com>
2021-11-03 21:37:47 +0800

    dataset

---
+++ builddataset.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib> // Header file needed to use srand and rand
+#include <ctime> // Header file needed to use time
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	
+	int usernum = 250;
+	
+	ofstream file;
+	file.open("d:\\dataset.txt");
+	
+	int cul = 40;
+	
+	int seed = time(0);
+	for (int i=1;i <= 100;i++) {
+		for (int j=1;j <= cul;j++) {
+			int dice = rand() % 172 + 1;
+			file << i << "\t" << dice << endl; 
+		}
+	}
+	
+	return 0;
+ } 
